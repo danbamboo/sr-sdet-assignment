ffa98f596604562e4829a0f144c5d1a298d03a4a
Danny McMurrough <mcmurroug1hdan@gmail.com>
2023-12-01 16:18:11 -0700

    update contracts

--- dashboard-api/test/contract-testing/billing/billing_service_client_test.rb
+++ dashboard-api/test/contract-testing/billing/billing_service_client_test.rb
@@ -19,8 +19,11 @@ class BillingServiceClientTestPact < Minitest::Test
 
   #Consumer-Driven-Pact to validate a successful call to get billing subscription (/subscriptions?user_id=1)
   def test_successful_pact_get_events
-    @success_response_body = "{\"user_id\": 1, \"renewal_date\": \"11/03/2023\", \"price_cents\": 1500}"
-    @parsed_success_response = JSON.parse(@success_response_body)
+    @parsed_success_response = {
+      user_id: 1,
+      renewal_date: Pact.like("11/03/2023"),
+      price_cents: 1500
+    }
 
     billing_service_pact
       .given("Billing subscriptions exist for user ID")
@@ -51,64 +54,3 @@ class BillingServiceClientTestPact < Minitest::Test
     assert  BillingServiceClient.new().get_billing_subscriptions_fail
   end
 end
-
-
-
-
-# class Contract::BillingPactTest < ActiveSupport::TestCase
-
-# describe BillingServiceClient, :pact => true do
-#   include Pact::Consumer::Minitest
-
-#   def setup
-#     @billing_service = Api::BillingService.new(1)
-#   end
-
-#   describe "get_billing" do
-#     before do
-#       animal_service.given("an billing event exists").
-#         upon_receiving("a request for an billing event").
-#         with(method: :get, path: '/event', query: 'user_id=2').
-#         will_respond_with(
-#           status: 200,
-#           headers: {'Content-Type' => 'application/json'},
-#           body: {name: 'Betty'} )
-#     end
-
-#     it "returns an event" do
-#       expect(subject.get_cal_event).to eq(BillingService.new('Betty'))
-#     end
-#   end
-# end
-
-
-
-
-
-
-#   include Pact::Consumer::Minitest
-#   Pact.service_consumer "dashboard_api" do  # register a consumer with pact
-#     has_pact_with "billing_service" do   # register the provider that has the pact
-#       mock_service :billing_service do # register the mock service that will run and pretend to be the provider
-#         port 8000
-#       end
-#     end
-#   end
-
-#   Pact.configure do |config|
-#     config.pact_dir = './test/pacts'
-#   end
-
-#   def test_pact
-#     billing_service
-#       .given('a billing event exists')
-#       .upon_receiving('a request for billing event')
-#       .with(method: :get, path: '/event', query: 'user_id=2')
-#       .will_respond_with(status: 200)
-
-#     HTTPClient.new.get_content('http://localhost:8000')
-#   end
-# end
-
-
-
