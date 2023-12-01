5c52777bffaeca8546895f169283bf675aa23890
Danny McMurrough <mcmurroug1hdan@gmail.com>
2023-11-30 23:30:55 -0700

    WIP on user-service contract validation

---
+++ user-service/test/contract/pact_helper.rb
@@ -0,0 +1,8 @@
+require 'pact/provider/rspec'
+
+Pact.service_provider "user_service" do
+  honours_pact_with 'dashboard_api' do
+    # TODO - Add pack broker so some non-local file access
+    pact_uri  '../assignment_submission/contract_pacts/dashboard_api-user_service.json'
+  end
+end
