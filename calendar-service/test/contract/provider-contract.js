5e8ac22cba1309895670acd0043afb65819752f5
Danny McMurrough <mcmurroug1hdan@gmail.com>
2023-12-03 15:45:08 -0700

    calendar server run as unit

--- calendar-service/test/contract/provider-contract.js
+++ calendar-service/test/contract/provider-contract.js
@@ -1,15 +1,21 @@
+
+
 import path from 'path';
 import { Verifier } from "@pact-foundation/pact";
 import { fileURLToPath } from 'url';
+import testPort from './set-env.js'
+import server from '../../app.js'  //Will run server on testPort for unit type testing
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//Validates a consumer generated contract (from dashboard-api) using local pact file against live running services
+//Validates a consumer generated contract (from dashboard-api) using local pact file against server this test stands up
 //Requires calendar-service to be running
 //-->Run with `npm test`
 
 new Verifier({
-  providerBaseUrl: `http://localhost:8000`,
+  providerBaseUrl: `http://localhost:${testPort}`,
   pactUrls: [path.resolve(__dirname, '../../../assignment_submission/contract_pacts/dashboard_api-calendar_service.json')],
-}).verifyProvider()
+}).verifyProvider().then(setTimeout(function(){server.close()}, 5000))  //Shutdown server after 5 seconds to let test run
