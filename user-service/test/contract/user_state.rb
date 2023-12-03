c0d73f68541c91b425511da1cd93a2cb6dc55f7a
Danny McMurrough <mcmurroug1hdan@gmail.com>
2023-12-02 22:32:17 -0700

    contract makefile complete

--- user-service/test/contract/user_state.rb
+++ user-service/test/contract/user_state.rb
@@ -3,6 +3,7 @@ Pact.provider_states_for 'dashboard_api' do
   #Currently no updates to the 2 required "states" for contracts to pass.
   
     provider_state "User subscriptions exist for user ID" do
+      no_op # No actions requred for error state
       #No setup required due to hardcoded data
     end
   
